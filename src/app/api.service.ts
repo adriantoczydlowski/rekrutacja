effc30a52278d877b89cc9d4f60ce3af82ca9195
Adrian Toczydlowski <atoczydlowski@ownedoutcomes.com>
2017-01-27 16:33:39 +0100

    ApiService, proxy config

---
+++ src/app/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+
+const BASE_URL = '/api';
+const user = 'rekrutacja';
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: Http) { }
+
+  getProfile() {
+    return this.http.get(`${BASE_URL}/posts?author=${user}`);
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './api.service';
+
+interface Post {
+  id: number;
+  title: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'rekrutacja';
+  dataIsAvailable: boolean;
+  posts: Post[];
+
+  constructor(private apiService: ApiService) {
+    this.dataIsAvailable = false;
+  }
+
+  ngOnInit() {
+    this.apiService.getProfile()
+      .subscribe(posts => {
+        this.posts = posts.json();
+        this.dataIsAvailable = true;
+      });
+  }
 }
